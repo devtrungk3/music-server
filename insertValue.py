45df3da6d4812639e7080a257db49a8d740cc68d
trungHacker2k3 <ndtrung30@gmail.com>
2024-05-18 16:43:34 +0700

    feat: update python tool for insert data from csv to mysql

--- insertValue.py
+++ insertValue.py
@@ -15,13 +15,31 @@ except mysql.connector.Error as err:
     print("Error connecting to database:", err)
     exit()
     
-table_name = "songs"
-insert_query = f"INSERT INTO {table_name} {'(' + ','.join(df.columns) + ')'} VALUES ({'%s,'*(df.columns.size-1)}%s)"
+# table_name = "songs"
+# insert_query = f"INSERT INTO {table_name} {'(' + ','.join(df.columns) + ')'} VALUES ({'%s,'*(df.columns.size-1)}%s)"
 
-for row in df.values:
-    cursor.execute(insert_query, tuple(row))
-    connection.commit()
-print("inserted successfully")
+# for row in df.values:
+#     cursor.execute(insert_query, tuple(row))
+#     connection.commit()
+# print("inserted successfully")
+
+for i in range(0, len(df["genres"])):
+    
+    releasedYear = df["releasedYear"][i]
+    select_query = f"SELECT id FROM songs WHERE title=%s AND releasedYear={releasedYear}"
+    cursor.execute(select_query, (df["title"][i],))
+    song = cursor.fetchone()
+    
+    arr = df["genres"][i].split(', ')
+    select_query = f"SELECT id FROM genres WHERE {'title=%s OR '*(len(arr)-1)}title=%s"
+    cursor.execute(select_query, tuple(arr))
+    genres = cursor.fetchall()
+    middle = [(song[0], genre[0]) for genre in genres]
+    
+    for row in middle:     
+        insert_query = f"INSERT INTO songs_genres (songId, genreId) VALUES (%s,%s)"
+        cursor.execute(insert_query, tuple(row))
+        connection.commit()
     
 if connection:
     cursor.close()
